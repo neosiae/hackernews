e4cf3a68740988f8e53e5a8a259d4387002fde10
Denis Kotlica <neosiae@protonmail.com>
2020-01-10 13:05:58 +0100

    Add nullish coalescing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,6 @@ import express from 'express'
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
