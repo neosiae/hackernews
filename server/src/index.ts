f84b6ee182b4b3c3a1327d2142c2387dfc37bf01
Denis Kotlica <neosiae@protonmail.com>
2020-01-11 16:53:41 +0100

    Add typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,18 @@
-import express from 'express'
+import "reflect-metadata"
+import express, { Application } from 'express'
 import bodyParser from 'body-parser'
+import { createConnection } from 'typeorm'
 
-const app = express()
+const PORT: string = process.env.PORT ?? '3000'
+
+const app: Application = express()
 
 app.use(bodyParser.json())
 
-const PORT = process.env.PORT ?? 3000
+const init = async () => {
+  await createConnection()
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+  app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+}
+
+init()
