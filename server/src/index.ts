dae156544d1f9b1c5725e136a817fa6945245628
Denis Kotlica <neosiae@protonmail.com>
2020-01-13 19:38:10 +0100

    Add User routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,14 @@ import "reflect-metadata"
 import express, { Application } from 'express'
 import bodyParser from 'body-parser'
 import { createConnection } from 'typeorm'
+import userRoutes from './routes/user'
 
 const PORT: string = process.env.PORT ?? '3000'
 
 const app: Application = express()
 
 app.use(bodyParser.json())
+app.use('/api', userRoutes)
 
 const init = async () => {
   await createConnection()
---
+++ server/src/routes/user.ts
@@ -0,0 +1,59 @@
+import { Router, Request, Response } from 'express'
+import { getManager } from 'typeorm'
+import bcrypt from 'bcryptjs'
+import User from '../entity/User'
+
+const router: Router = Router()
+
+interface IUser {
+  username: string,
+  email: string,
+  password: string
+}
+
+router.post('/user/create', async (req: Request, res: Response) => {
+  const { username, email, password }: IUser = req.body
+  const userRepository = getManager().getRepository(User)
+
+  try {
+    const existingUsername = await userRepository.findOne({ username })
+    
+    if (existingUsername) {
+      res.status(409).json({
+        message: 'An account with this username already exists.'
+      })
+    }
+
+    const existingEmail = await userRepository.findOne({ email })
+
+    if (existingEmail) {
+      res.status(409).json({
+        message: 'An account with this email already exists.'
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const user = userRepository.create({ username, email, password: hashedPassword })
+
+    try {
+      await userRepository.save(user)
+
+      res.status(201).json({
+        message: 'User successfully created.'
+      })
+    } catch (err) {
+      console.error(err)
+      res.sendStatus(500)
+    }
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
+})
+
+export default router
