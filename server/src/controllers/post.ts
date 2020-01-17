817cce623e99aa3ff096ea00aed7f3e9fb85a356
Denis Kotlica <neosiae@protonmail.com>
2020-01-17 18:23:46 +0100

    Add orderBy

--- server/src/controllers/post.ts
+++ server/src/controllers/post.ts
@@ -50,6 +50,7 @@ export async function getPosts (req: Request, res: Response) {
     .createQueryBuilder(Post, 'post')
     .innerJoin("post.author", "author")
     .addSelect("author.username")
+    .orderBy("post.createdAt", "DESC")
     .getMany()
 
     res.status(200).json(posts)
