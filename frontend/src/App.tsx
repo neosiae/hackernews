bf1bf47e159625889ac913a077304c814ed57a9b
Denis Kotlica <neosiae@protonmail.com>
2020-01-24 18:01:42 +0100

    Add nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Nav from './components/Nav'
 import Signup from './screens/Signup'
 import './app.css'
 
@@ -7,6 +8,7 @@ function App () {
   return (
     <div className='container'>
       <Router>
+        <Nav />
         <Switch>
           <Route path='/signup'>
             <Signup />
