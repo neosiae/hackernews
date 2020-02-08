05614e13861acdd01dce6e71f4c504b480035f80
Denis Kotlica <neosiae@protonmail.com>
2020-02-08 16:14:10 +0100

    Remove semicolons

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
